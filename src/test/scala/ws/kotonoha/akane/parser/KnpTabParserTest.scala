a53dedc7701de9f15e6764324bffeab91bd47e93
Arseny Tolmachev <eiennohito@gmail.com>
2014-11-26 18:07:38 +0900

    updated test for knp tab parser test

--- src/test/scala/ws/kotonoha/akane/parser/KnpTabParserTest.scala
+++ src/test/scala/ws/kotonoha/akane/parser/KnpTabParserTest.scala
@@ -1,6 +1,6 @@
 package ws.kotonoha.akane.parser
 
-import org.scalatest.FreeSpec
+import org.scalatest.{Matchers, FreeSpec}
 import org.scalatest.matchers.ShouldMatchers
 import scalax.io.Resource
 
@@ -8,14 +8,14 @@ import scalax.io.Resource
  * @author eiennohito
  * @since 2014-05-08
  */
-class KnpTabParserTest extends FreeSpec with ShouldMatchers {
+class KnpTabParserTest extends FreeSpec with Matchers {
   "KnpTabParser" - {
     val parser = new KnpTabFormatParser
     "parses a small tree" in {
       val lines = Resource.fromClasspath("knp.tab.txt").lines()
       val result = parser.parse(lines)
-      result.bunsetsu should have length(11)
-      result.bunsetsu(10).lexemes.head.reading should be("ある")
+      result.bunsetsu should have length(5)
+      result.bunsetsu(4).lexemes.head.reading should be("み")
     }
   }
 }
