2e62dbbb150e0394efcb4ed1a20ef8110b2e6f1f
Arseny Tolmachev <eiennohito@gmail.com>
2015-08-15 15:19:27 +0900

    add usable juman pos info
    with mapping from ints to pos!
    and other information like conjuations and stuff

---
+++ src/main/scala/ws/kotonoha/akane/parser/JumanPosSet.scala
@@ -0,0 +1,131 @@
+package ws.kotonoha.akane.parser
+
+import ws.kotonoha.akane.pipe.knp.lisp._
+
+import scala.util.parsing.input.CharSequenceReader
+import scalax.io.{Codec, Resource}
+
+/**
+ * @param subtypes
+ * @author eiennohito
+ * @since 15/08/15
+ */
+case class JumanPosType(num: Int, name: String, subtypes: Array[JumanPosSubtype])
+
+case class JumanPosSubtype(num: Int, name: String, possibleConjs: Array[Int])
+
+case class JumanConjType(num: Int, name: String, conjugations: Array[JumanConjForm])
+
+case class JumanConjForm(num: Int, name: String, writing: String, reading: String)
+
+class JumanPosSet(
+  val pos: Array[JumanPosType],
+  val conjugatons: Array[JumanConjType]
+)
+
+object JumanPosSet {
+  lazy val default = JumanPosReader.fromClasspath()
+}
+
+object JumanPosReader {
+  private def parseLisp(cont: String): List[KList] = {
+    val input = new CharSequenceReader(cont)
+    LispParser.lists(input) match {
+      case LispParser.Success(x, _) => x
+      case y => throw new JumanPosException("could not parse file:\n" + y + "\n\n" + cont)
+    }
+  }
+
+  def fromClasspath() = {
+    val conjFile = Resource.fromClasspath("juman/JUMAN.katuyou").string(Codec.UTF8)
+    val gramFile = Resource.fromClasspath("juman/JUMAN.grammar").string(Codec.UTF8)
+    val relFile = Resource.fromClasspath("juman/JUMAN.kankei").string(Codec.UTF8)
+
+    fromLispAst(parseLisp(gramFile), parseLisp(relFile), parseLisp(conjFile))
+  }
+
+  def fromLispAst(grammar: List[KList], kankei: List[KList], katuyou: List[KList]) = {
+    val cobj = parseKatuyou(katuyou)
+    val cmap = cobj.map {
+      case (k, v) => k -> v.toArray
+    }.toMap
+
+    val ckeys = "*" :: cobj.map(_._1)
+
+    val carray = ckeys.zipWithIndex.map {
+      case (k, i) => cmap.get(k).map(a => JumanConjType(i, k, a)).getOrElse(
+        JumanConjType(i, k, Array(JumanConjForm(0, "*", "", "")))
+      )
+    }
+
+    val cmap2 = carray.map{o => o.name -> o.num}.toMap
+
+    val kankobj = parseKankei(kankei)
+
+    val posobj = ("*" -> List("*" -> false)) :: parseGrammar(grammar)
+
+    val kanMap = kankobj.toMap
+
+    val pobjs = posobj.zipWithIndex.map{
+      case ((k, l), i) => JumanPosType(i, k, l.zipWithIndex.map {
+        case ((nm, flag), j) =>
+          val cobjs = kanMap.getOrElse((k, nm), Nil).map(cmap2).toArray
+          JumanPosSubtype(j, nm, cobjs)
+      }.toArray)
+    }
+
+    new JumanPosSet(pobjs.toArray, carray.toArray)
+  }
+
+  def conjugations(pairs: List[KElement]) = {
+    var idx = 1
+    pairs.map {
+      case KItems(KAtom(l), KAtom(r)) =>
+        val c = JumanConjForm(idx, l, r, r)
+        idx += 1
+        c
+      case KItems(KAtom(l), KAtom(w), KAtom(r)) =>
+        val c = JumanConjForm(idx, l, w, r)
+        idx += 1
+        c
+      case x =>
+        throw new JumanPosException("unknown format\n" + x)
+    }
+  }
+
+  def parseKatuyou(lisp: List[KList]) = {
+    lisp.map {
+      case KItems(KAtom(name), KList(pairs)) => name -> (JumanConjForm(0, "*", "", "") :: conjugations(pairs))
+      case x => throw new JumanPosException("unknown format\n" + x)
+    }
+  }
+
+  def parseGrammar(lst: List[KList]) = {
+    lst.map {
+      case KItems(KItems(KAtom(nm))) => nm -> List(("*", false))
+      case KItems(KItems(KAtom(nm), KAtom("%"))) => nm -> List(("*", true))
+      case KItems(KItems(KAtom(nm)), KList(itms)) => nm -> (
+        ("*", false) :: itms.map {
+          case KItems(KAtom(s)) => (s, false)
+          case KItems(KAtom(s), KAtom("%")) => (s, true)
+        })
+    }
+  }
+
+  def parseKankei(lst: List[KList]) = lst map {
+    case KItems(KItems(KAtom(key1)), KList(items)) =>
+      (key1, "*") -> ("*" :: items.map {
+        case KAtom(cnt) => cnt
+        case x => throw new JumanPosException("unknown format\n" + x)
+      })
+    case KItems(KItems(KAtom(key1), KAtom(key2)), KList(items)) =>
+      (key1, key2) -> ("*" :: items.map {
+        case KAtom(cnt) => cnt
+        case x => throw new JumanPosException("unknown format\n" + x)
+      })
+    case x =>
+      throw new JumanPosException("unknown format\n" + x)
+  }
+}
+
+class JumanPosException(s: String) extends RuntimeException(s)
--- src/test/scala/ws/kotonoha/akane/parser/JumanPosInfoSpec.scala
+++ src/test/scala/ws/kotonoha/akane/parser/JumanPosInfoSpec.scala
@@ -23,7 +23,26 @@ class JumanPosInfoSpec extends FreeSpec with Matchers {
     }
   }
 
-  "JumanPosParser" - {
+  def checkPos(set: JumanPosSet, tr: KnpTable): Unit = {
+    for (l <- tr.lexemes) {
+      val p = l.pos
+      val pos = p.pos
+      val px = set.pos(pos.id)
+      pos.name shouldBe px.name
+
+      val sub = p.category
+      sub.name shouldBe px.subtypes(sub.id).name
+
+      val conjTp = p.conjType
+      val ctx = set.conjugatons(conjTp.id)
+      conjTp.name shouldBe ctx.name
+
+      val conjFrm = p.conjForm
+      conjFrm.name shouldBe ctx.conjugations(conjFrm.id).name
+    }
+  }
+
+  "JumanPosReader" - {
     "parses katuyou" in {
       val lst = parseLisp("juman/JUMAN.katuyou")
       lst should not be ('empty)
@@ -31,6 +50,43 @@ class JumanPosInfoSpec extends FreeSpec with Matchers {
       obj should not be('empty)
       obj should have length 32
       obj(31)._1 shouldBe "動詞性接尾辞うる型"
+    }
+
+    "parses kankei" in {
+      val lst = parseLisp("juman/JUMAN.kankei")
+      lst should not be('empty)
+      val obj = JumanPosReader.parseKankei(lst)
+      obj should have length 7
+      obj(6)._1 shouldBe ("接尾辞", "動詞性接尾辞")
+    }
+
+    "parses grammar" in {
+      val lst = parseLisp("juman/JUMAN.grammar")
+      val obj = JumanPosReader.parseGrammar(lst)
+      obj should have length 15
+    }
+  }
+
+  "default JumanPosSet" - {
+    lazy val set = JumanPosSet.default
+    "correctly loads" in {
+      set.pos should have length 16
+      set.conjugatons should have length 33
+    }
+
+    "and matches all pos tags" - {
+      "tree: tree0" in {
+        val tr = TreeUtil.classpath("trees/bunsetsu-1.txt")
+        checkPos(set, tr)
+      }
+
+      "tree: tree1" in {
+        checkPos(set, TreeUtil.classpath("trees/weirdKihonku.txt"))
+      }
+
+      "tree: tree2" in {
+        checkPos(set, TreeUtil.classpath("knp.tab.txt"))
+      }
     }
   }
 }
