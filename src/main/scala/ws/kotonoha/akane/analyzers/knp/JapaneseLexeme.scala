0048451477ee00bb2f98cf1f1cc819a6abc8d261
Arseny Tolmachev <eiennohito@gmail.com>
2016-01-09 15:47:36 +0900

    lexical difficulty estimation

--- src/main/scala/ws/kotonoha/akane/analyzers/knp/JapaneseLexeme.scala
+++ src/main/scala/ws/kotonoha/akane/analyzers/knp/JapaneseLexeme.scala
@@ -6,11 +6,14 @@ import ws.kotonoha.akane.analyzers.juman.JumanStylePos
  * @author eiennohito
  * @since 2015/09/18
  */
-trait JapaneseLexeme extends FeatureAccess {
+trait JapaneseLexeme extends FeatureAccess with PosAccess {
   def surface: String
   def reading: String
   def dicForm: String
-  def pos: JumanStylePos
   //def tags: Seq[String]
   def canonicForm(): String
+}
+
+trait PosAccess {
+  def pos: JumanStylePos
 }
