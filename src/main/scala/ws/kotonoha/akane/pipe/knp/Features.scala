80b9645c401285af789f8824eb86fcc3e7cecad3
Arseny Tolmachev <eiennohito@gmail.com>
2016-01-14 19:40:36 +0900

    add feature constants

--- src/main/scala/ws/kotonoha/akane/pipe/knp/Features.scala
+++ src/main/scala/ws/kotonoha/akane/pipe/knp/Features.scala
@@ -1,15 +1,19 @@
 package ws.kotonoha.akane.pipe.knp
 
-import ws.kotonoha.akane.analyzers.knp.FeatureAccess
+import ws.kotonoha.akane.analyzers.knp.{KihonkuApi, FeatureAccess}
 
 /**
  * @author eiennohito
  * @since 15/07/14
  */
 object Features {
-  val predArg = "格解析結果"
 
+  val yougen: String = "用言"
+  val prefix: String = "接頭"
+  val predArg = "格解析結果"
   val normalizedWriting = "正規化代表表記"
+
+  def isYougen(api: FeatureAccess): Boolean = api.featureExists(yougen)
 
   def normWriting(floc: FeatureAccess) = {
     floc.valueOfFeature(normalizedWriting)
