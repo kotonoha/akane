7e816be62a52a5b5c8408817a489353ffe973092
Arseny Tolmachev <eiennohito@gmail.com>
2016-08-11 21:23:56 +0900

    move timers to akane-util

---
+++ util/src/main/scala/ws/kotonoha/akane/utils/timers/Timers.scala
@@ -0,0 +1,26 @@
+package ws.kotonoha.akane.utils.timers
+
+final class Millitimer {
+  private def time() = System.currentTimeMillis()
+  val start = time()
+  def eplaced = Millis(time() - start)
+}
+
+final case class Millis(millis: Long) extends AnyVal {
+  @inline def seconds = millis * 1e-3
+}
+
+object Millitimer {
+  def apply() = new Millitimer
+}
+
+final class Nanotimer {
+  val start = System.nanoTime()
+  def eplaced = System.nanoTime() - start
+
+  @inline def millis = f"${eplaced * 1e-6}%.3f"
+}
+
+object Nanotimer {
+  def apply() = new Nanotimer
+}
